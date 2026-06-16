25680eab0ce9d75873e6ed40db0dea967ee40489
Hrishikesh Jadhav <hrishikesh.shivaji.jadhav@gmail.com>
2026-06-16 12:22:36 +0530

    Create 03 Using the Var Type Identifier.java

---
+++ 03 Using the Var Type Identifier.java
@@ -0,0 +1,18 @@
+String message = "Hello world!";
+Path path = Path.of("debug.log");
+InputStream stream = Files.newInputStream(path);
+
+
+public class User  {
+    private var name = "Sue"; // COMPILER ERROR
+
+    public void setName(var name) {
+        this.name = name;
+    }
+    var greetings; // COMPILER ERROR
+    if (message == 0) {
+        greetings = "morning";
+    } else {
+        greetings = "afternoon";
+}
+
